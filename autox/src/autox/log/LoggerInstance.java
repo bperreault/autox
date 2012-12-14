26b60d80a9cd598de6dbf4cd03a16218d5cd4cdb
jien.huang <jien.huang@gmail.com>
2012-12-14 10:00:25 +0000

    minor update on spell, etc

--- autox/src/autox/log/LoggerInstance.java
+++ autox/src/autox/log/LoggerInstance.java
@@ -1,9 +1,12 @@
 package autox.log;
 
 
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import java.io.File;
 
 
 public class LoggerInstance {
@@ -11,7 +14,14 @@ public class LoggerInstance {
 	private static LoggerInstance instance = new LoggerInstance();
 	private Logger log = Logger.getLogger("");
 	private LoggerInstance(){
-		PropertyConfigurator.configure("log4j.properties");
+        File properties = new File("./log4j.properties");
+        if(properties.exists()){
+            PropertyConfigurator.configure("log4j.properties");
+            System.out.println(properties.getAbsolutePath()+" is not existed, please check!");
+        }
+        else
+            BasicConfigurator.configure();
+
 	}
 	
 	public static LoggerInstance getInstance(){
--- autox/src/autox/tests/MainTest.java
+++ autox/src/autox/tests/MainTest.java
@@ -1,58 +1,56 @@
 package autox.tests;
 
-import java.security.KeyPairGenerator;
-import java.security.KeyPair;
-import java.security.PublicKey;
-import java.security.PrivateKey;
-import javax.crypto.Cipher;
-
-import org.apache.axis.encoding.Base64;
-import org.apache.james.mime4j.field.datetime.DateTime;
-import org.python.modules.time;
-
 import autox.log.Log;
+import org.apache.axis.encoding.Base64;
+
+import javax.crypto.Cipher;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 
 public class MainTest {
 
-	private static byte[] encrypt(byte[] inpBytes, PublicKey key,
-		      String xform) throws Exception {
-		    Cipher cipher = Cipher.getInstance(xform);
-		    cipher.init(Cipher.ENCRYPT_MODE, key);
-		    return cipher.doFinal(inpBytes);
-		  }
-		  private static byte[] decrypt(byte[] inpBytes, PrivateKey key,
-		      String xform) throws Exception{
-		    Cipher cipher = Cipher.getInstance(xform);
-		    cipher.init(Cipher.DECRYPT_MODE, key);
-		    return cipher.doFinal(inpBytes);
-		  }
+    private static byte[] encrypt(byte[] inpBytes, PublicKey key,
+                                  String xForm) throws Exception {
+        Cipher cipher = Cipher.getInstance(xForm);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(inpBytes);
+    }
 
-		  public static void main(String[] unused) throws Exception {
-		    String xform = "RSA";
-		    
-		    // Generate a key-pair
-		    Log.debug("Before generate key pair");
-		    
-		    KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-		    kpg.initialize(512); // 512 is the keysize.
-		    KeyPair kp = kpg.generateKeyPair();
-		    PublicKey pubk = kp.getPublic();
-		    PrivateKey prvk = kp.getPrivate();
+    private static byte[] decrypt(byte[] inpBytes, PrivateKey key,
+                                  String xForm) throws Exception {
+        Cipher cipher = Cipher.getInstance(xForm);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(inpBytes);
+    }
 
-		    System.out.println("publick key:"+pubk.toString());
-		    System.out.println("private key:"+prvk.toString());
-		    
-		    byte[] dataBytes =
-		        "Jien Huang is a good father and husband.".getBytes();
-Log.debug("before encrypt");
-		    byte[] encBytes = encrypt(dataBytes, pubk, xform);
-		    Log.debug("before decrypt");
-		    byte[] decBytes = decrypt(encBytes, prvk, xform);
+    public static void main(String[] unused) throws Exception {
+        String xForm = "RSA";
 
-		    boolean expected = java.util.Arrays.equals(dataBytes, decBytes);
-		    System.out.println(Base64.encode(encBytes));
-		    System.out.println("Test " + (expected ? "SUCCEEDED!" : "FAILED!"));
-		  }
+        // Generate a key-pair
+        Log.debug("Before generate key pair");
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(xForm);
+        kpg.initialize(512); // 512 is the key size.
+        KeyPair kp = kpg.generateKeyPair();
+        PublicKey publicKey = kp.getPublic();
+        PrivateKey privateKey = kp.getPrivate();
+
+        System.out.println("public key:" + publicKey.toString());
+        System.out.println("private key:" + privateKey.toString());
+
+        byte[] dataBytes =
+                "Jien Huang is a good father and husband.".getBytes();
+        Log.debug("before encrypt");
+        byte[] encBytes = encrypt(dataBytes, publicKey, xForm);
+        Log.debug("before decrypt");
+        byte[] decBytes = decrypt(encBytes, privateKey, xForm);
+        Log.debug("end of decrypt");
+        boolean expected = java.util.Arrays.equals(dataBytes, decBytes);
+        System.out.println(Base64.encode(encBytes));
+        System.out.println("Test " + (expected ? "SUCCEEDED!" : "FAILED!"));
+    }
 
 }
