3f27f2ba2b0c26fce8d8e09781c1f9374269ec0d
jien.huang <jien.huang@gmail.com>
2012-12-17 13:58:48 +0000

    test after merge

--- autox/src/autox/Start.java
+++ autox/src/autox/Start.java
@@ -3,7 +3,11 @@ package autox;
 import autox.actions.ActionFactory;
 import autox.communication.Communication;
 import autox.config.Configuration;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.jdom.Element;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Start {
 
@@ -14,7 +18,7 @@ public class Start {
         // if run.mode = test , start command line mode to test
         String mode = Configuration.getInstance().get("run.mode","run");
         if(mode.equalsIgnoreCase("test")){
-
+            test();
         }   else{
             // TODO read the configuration, connect to autox server, read command and run command
             while (true){
@@ -27,5 +31,57 @@ public class Start {
 
 
 	}
+
+    private static void test() {
+        while (true){
+            int choice = showMenu();
+            switch (choice) {
+                case 0:
+                    return;
+                default:
+                    Write("\nPlease input a valid choice\n");
+                    break;
+            }
+        }
+    }
+
+    private static int showMenu() {
+        Write("\n\nSimple Tests for AutoX Client\n");
+        Write("0. Exit");
+
+        Write("\n");
+        Write("Please input your choice:");
+        try {
+
+            return Integer.parseInt(ReadInput());
+        } catch (Exception e) {
+
+            Write(e.getMessage() + "\n" + ExceptionUtils.getStackTrace(e));
+        }
+        return -1;
+    }
+
+
+    private static String ReadInput(String message) {
+        Write("\n" + message);
+        return ReadInput();
+    }
+
+    private static String ReadInput() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            return br.readLine();
+        } catch (Exception e) {
+
+            Write(e.getMessage() + "\n" + ExceptionUtils.getStackTrace(e));
+        }
+        return "-1";
+    }
+
+
+    private static void Write(String message) {
+        System.out.println(message);
+
+    }
 
 }
