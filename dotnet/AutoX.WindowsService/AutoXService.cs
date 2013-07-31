9b2d42ef859417e01c4344ed9a02c2a95c3709ef
huangjien <huangjien@gmail.com>
2013-07-31 09:30:04 +0100

    windows service host web server OK

--- dotnet/AutoX.WindowsService/AutoXService.cs
+++ dotnet/AutoX.WindowsService/AutoXService.cs
@@ -9,80 +9,27 @@ namespace AutoX.WindowsService
 {
     public partial class AutoXService : ServiceBase
     {
+        private HttpListenerController _controller;
+
         public AutoXService()
         {
             InitializeComponent();
         }
 
-        HttpListener listener = new HttpListener();
-        private Thread listenThread1;
-
         protected override void OnStart(string[] args)
         {
-            Log.Debug("AutoX Windows Service Start ...");
-            listener.Prefixes.Add("http://localhost/");
-            listener.Start();
-            Log.Debug("Listener start ...");
-            this.listenThread1 = new Thread(new ParameterizedThreadStart(startlistener));
-            listenThread1.Start();
-            Log.Debug("Listener thread start ...");
-            Thread.Sleep(60 * 5 * 1000);
-        }
+            string[] prefixes = new string[] {
+                "http://localhost:8081/AutoX.Web/",
+                "http://127.0.0.1:8081/AutoX.Web/",
+            };
+            _controller = new HttpListenerController(prefixes, "/AutoX.Web", @"C:\inetpub\wwwroot\AutoX.Web");
+            _controller.Start();
 
-        private void startlistener(object s)
-           {
-   
-               while (true)
-               {
-                  
-                   ////blocks until a client has connected to the server
-                   ProcessRequest();
-   
-               }
-   
-           }
-   
-   
-           private void ProcessRequest()
-           {
-   
-               var result = listener.BeginGetContext(ListenerCallback, listener);
-               result.AsyncWaitHandle.WaitOne();
-   
-           }
-   
-           private void ListenerCallback(IAsyncResult result)
-           {
-               var context = listener.EndGetContext(result);
-               Thread.Sleep(1000);
-               var data_text = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding).ReadToEnd();
-   
-               //functions used to decode json encoded data.
-               //JavaScriptSerializer js = new JavaScriptSerializer();
-               //var data1 = Uri.UnescapeDataString(data_text);
-               //string da = Regex.Unescape(data_text);
-               // var unserialized = js.Deserialize(data_text, typeof(String));
-               Log.Debug(data_text);
-   
-               var cleaned_data = System.Web.HttpUtility.UrlDecode(data_text);
-   
-               context.Response.StatusCode = 200;
-               context.Response.StatusDescription = "OK";
-   
-               //use this line to get your custom header data in the request.
-               //var headerText = context.Request.Headers["mycustomHeader"];
-   
-               //use this line to send your response in a custom header
-               //context.Response.Headers["mycustomResponseHeader"] = "mycustomResponse";
-               
-               Log.Debug(cleaned_data);
-               context.Response.Close();
-           }
+        }
 
         protected override void OnStop()
         {
-            Log.Debug("AutoX Windows Service Stop ...");
-            listener.Stop();
+            _controller.Stop();
         }
     }
 }
---
+++ dotnet/AutoX.WindowsService/HttpListenerController.cs
@@ -0,0 +1,107 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Web;
+using System.Web.Hosting;
+using System.Threading;
+using System.Diagnostics;
+using AutoX.Basic;
+#endregion
+
+namespace AutoX.WindowsService
+{
+    public class HttpListenerController
+    {
+        private Thread _pump;
+        private bool _listening = false;
+        private string _virtualDir;
+        private string _physicalDir;
+        private string[] _prefixes;
+        private HttpListenerWrapper _listener;
+
+        public HttpListenerController(string[] prefixes, string vdir, string pdir)
+        {
+            _prefixes = prefixes;
+            _virtualDir = vdir;
+            _physicalDir = pdir;
+        }
+
+        public void Start()
+        {
+            _listening = true;
+            _pump = new Thread(new ThreadStart(Pump));
+            _pump.Start();
+        }
+
+        public void Stop()
+        {
+            _listening = false;
+
+            Process.GetCurrentProcess().Kill();
+            _pump.Abort();
+            _pump.Join();
+        }
+
+        private void Pump()
+        {
+            try
+            {
+                Log.Debug("Before CreateApplicationHost");
+                
+                _listener = (HttpListenerWrapper)ApplicationHost.CreateApplicationHost(
+                    typeof(HttpListenerWrapper), _virtualDir, _physicalDir);
+                Log.Debug("Create CreateApplicationHost OK");
+                _listener.Configure(_prefixes, _virtualDir, _physicalDir);
+                _listener.Start();
+
+                while (_listening)
+                    _listener.ProcessRequest();
+            }
+            catch (Exception ex)
+            {
+                EventLog myLog = new EventLog();
+                myLog.Source = "HttpListenerController";
+                if (null != ex.InnerException)
+                    myLog.WriteEntry(ex.InnerException.ToString(), EventLogEntryType.Error);
+                else
+                    myLog.WriteEntry(ex.ToString(), EventLogEntryType.Error);
+            }
+        }
+    }
+
+    public class HttpListenerWrapper : MarshalByRefObject
+    {
+        private HttpListener _listener;
+        private string _virtualDir;
+        private string _physicalDir;
+
+        public void Configure(string[] prefixes, string vdir, string pdir)
+        {
+            _virtualDir = vdir;
+            _physicalDir = pdir;
+            _listener = new HttpListener();
+
+            foreach (string prefix in prefixes)
+                _listener.Prefixes.Add(prefix);
+        }
+        public void Start()
+        {
+            _listener.Start();
+        }
+        public void Stop()
+        {
+            _listener.Stop();
+        }
+        public void ProcessRequest()
+        {
+            HttpListenerContext ctx = _listener.GetContext();
+            HttpListenerWorkerRequest workerRequest =
+                new HttpListenerWorkerRequest(ctx, _virtualDir, _physicalDir);
+            HttpRuntime.ProcessRequest(workerRequest);
+        }
+    }
+
+
+}
