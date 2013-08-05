b6a28d19e66f9566e9aeab47ca3abd12c40ff6f9
huangjien <huangjien@gmail.com>
2013-08-05 15:16:34 +0100

    don't know

---
+++ dotnet/AutoX.WF.Core/StatusTracker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Activities.Tracking;
+
+namespace AutoX.WF.Core
+{
+    public sealed class StatusTracker : TrackingParticipant
+    {
+        private readonly TrackingProfile _trackingProfile = new TrackingProfile();
+
+        public StatusTracker()
+        {
+            _trackingProfile.Queries.Add(new ActivityStateQuery
+            {
+                ActivityName = "*",
+                States = { "*" },
+                Variables = { "*" },
+                Arguments = { "*" }
+            });
+            _trackingProfile.Queries.Add(new WorkflowInstanceQuery
+            {
+                States = { "*" },
+            });
+
+            TrackingProfile = _trackingProfile;
+        }
+
+        public string Status { get; private set; }
+
+        protected override void Track(TrackingRecord record, TimeSpan timeout)
+        {
+            var instanceRecord = record as WorkflowInstanceRecord;
+            if (instanceRecord != null)
+            {
+                Status = instanceRecord.State;
+            }
+        }
+    }
+}
